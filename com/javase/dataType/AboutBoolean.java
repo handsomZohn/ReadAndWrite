749ce79fe5f14b137e2778b6041b310174afcf2c
zohn <zzohn@foxmail.com>
2020-01-06 15:26:59 +0800

    feat(location):新增一点东西~~

--- com/javase/dataType/AboutBoolean.java
+++ com/javase/dataType/AboutBoolean.java
@@ -2,18 +2,25 @@ package com.javase.dataType;
 
 public class AboutBoolean {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		if(true || false){
-			System.out.println("false----");
-		}
-		
-		Boolean valueOf = Boolean.valueOf(false);
-		
-		System.out.println(valueOf);
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        if (true || false) {
+            System.out.println("false----");
+        }
+
+        Boolean valueOf = Boolean.valueOf(false);
+
+        System.out.println(valueOf);
+
+        /**
+         * 用==比较和.equals比较是相同的；
+         */
+        System.out.println(Boolean.FALSE == Boolean.TRUE);
+        System.out.println(Boolean.TRUE.equals(Boolean.FALSE));
+
+    }
 
 }

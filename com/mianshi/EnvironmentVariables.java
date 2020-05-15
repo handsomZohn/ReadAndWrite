61b6849343916f4bc00fd9f8d5a691a868360e67
zohn <zzohn@foxmail.com>
2020-05-15 15:00:12 +0800

    代码获取JAVA_HOME

---
+++ com/mianshi/EnvironmentVariables.java
@@ -0,0 +1,33 @@
+package com.mianshi;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+/**
+ * 获取os环境变量
+ * @author Administrator
+ */
+public class EnvironmentVariables {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        InputStream resourceAsStream = EnvironmentVariables.class.getResourceAsStream("./ev.properties");
+        try {
+            properties.load(new InputStreamReader(resourceAsStream, "utf-8"));
+            String key = "pwd";
+            if (properties.containsKey(key)) {
+                String property = properties.getProperty("pwd");
+                System.out.println("======^_^======variable property value is : " + property + ", " + " current method and class name is : EnvironmentVariables.main()");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        // 通过system访问
+        String java_home = System.getenv("JAVA_HOME");
+
+        System.out.println("======^_^======variable java_home value is : " + java_home + ", " + " current method and class name is : EnvironmentVariables.main()");
+
+    }
+}

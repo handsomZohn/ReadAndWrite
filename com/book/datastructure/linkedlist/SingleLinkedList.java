8abdbfd4b36cbc708a666e2f8bcceade1772b5e9
zohn <zzohn@foxmail.com>
2021-02-09 11:30:37 +0800

    双向链表Java实现

--- com/book/datastructure/linkedlist/SingleLinkedList.java
+++ com/book/datastructure/linkedlist/SingleLinkedList.java
@@ -1,13 +1,11 @@
 package com.book.datastructure.linkedlist;
 
 
-import java.text.SimpleDateFormat;
-
 /**
- *@Description 单向列表java实现
- *@CreateDate 2021\2\8 0008 14:00
- *@Author z
- *@Version 1.0
+ * @Description 单向列表java实现
+ * @CreateDate 2021\2\8 0008 14:00
+ * @Author z
+ * @Version 1.0
  */
 public class SingleLinkedList {
     /**
@@ -20,12 +18,12 @@ public class SingleLinkedList {
      */
     private Node head;
 
-    public SingleLinkedList(){
+    public SingleLinkedList() {
         length = 0;
         head = null;
     }
 
-    private class Node{
+    private class Node {
         /**
          * 每个节点的数据
          */
@@ -43,6 +41,7 @@ public class SingleLinkedList {
 
     /**
      * 链表中添加数据
+     *
      * @param object
      * @return
      */
@@ -59,16 +58,17 @@ public class SingleLinkedList {
             newHead.next = head;
         }
         // 第四步 链表长度+1
-        length ++;
+        length++;
         return object;
     }
 
     /**
      * 删除指定的元素 如果删除成功就返回true
+     *
      * @param value
      * @return
      */
-    public boolean delete(Object value){
+    public boolean delete(Object value) {
         if (length == 0) {
             return false;
         }
@@ -87,29 +87,30 @@ public class SingleLinkedList {
         // 如果删除的节点是头结点
         if (current == head) {
             head = current.next;
-            length --;
+            length--;
         } else {// 删除的节点不是头节点
             previous.next = current.next;
-            length --;
+            length--;
         }
         return true;
     }
 
     /**
      * 查找指定的元素，若找到则返回，否则返回null
+     *
      * @param object
      * @return
      */
-    public Node find(Object object){
+    public Node find(Object object) {
         Node current = head;
         int tempSize = length;
-        while (tempSize > 0){
+        while (tempSize > 0) {
             if (object.equals(current.data)) {
                 return current;
             } else {
                 current = current.next;
             }
-            tempSize --;
+            tempSize--;
         }
         return null;
     }
